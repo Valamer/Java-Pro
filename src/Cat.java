7ce448575366aab73bb03bc0203c339abc6b1e84
Valamer <Valamer.vlad@gmail.com>
2024-04-12 00:02:37 +0300

    Revert "Task #2"
    This reverts commit 4bb8a7838462a9ae739d6d33eab7b7a04c4f0a3d.

--- src/Cat.java
+++ src/Cat.java
@@ -1,11 +1,11 @@
 public class Cat extends Animal{
 
     private static int catCounter = 0;
+    double runDistance = 200;
+    double swimDistance = 0;
 
     public Cat(String name){
         this.name = name;
-        super.runDistance = 200;
-        super.swimDistance = 0;
         increaseAnimalCounter();
         catCounter++;
     }
