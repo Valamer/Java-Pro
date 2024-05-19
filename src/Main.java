cde88e5dfdbf5e7317d2e8158673e8e27cef7ba5
Valamer <Valamer.vlad@gmail.com>
2024-05-19 21:24:06 +0300

    Task #9 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+
+    public static boolean isPalindrome (String text) { // 1,2,2,4,5
+        text= text.replaceAll("[^a-zA-Z0-9]", "");
+        text= text.toLowerCase();
+        int left=0;
+        int right = text.length()-1;
+
+        while(left<right){
+            if(text.charAt(left)!= text.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
     }
+
+    public static boolean containDuplicate (int[] nums) {
+        HashSet<Integer> set  = new HashSet();
+        for (int num : nums) {
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static int[] twoSum (int[] nums, int target) {
+        Map<Integer, Integer> checkedNums = new HashMap<>();
+
+        for (int firstNum = 0; firstNum < nums.length; firstNum++) {
+            int secondNum = target - nums[firstNum];
+            if (checkedNums.containsKey(secondNum)) {
+                return new int[]{checkedNums.get(secondNum), firstNum};
+            }
+            checkedNums.put(nums[firstNum], firstNum);
+        }
+
+        return new int[0];
+    }
+
 }
